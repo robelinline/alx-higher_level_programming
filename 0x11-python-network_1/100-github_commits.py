d861d44d8ea256a2ccb220febcd0a595f83d3a14
robelinline <62613359+robelinline@users.noreply.github.com>
2024-04-26 21:13:16 +0300

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,7 +6,7 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
+    url = "https://api.github.com/repos/{}/commits".format(
         sys.argv[2], sys.argv[1])
 
     r = requests.get(url)
